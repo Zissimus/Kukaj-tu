43d315971675a47d9c0ad27a4ec7910e399c9d31
Zissimus <62388891+Zissimus@users.noreply.github.com>
2020-03-25 15:15:05 +0100

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        'Test KHT',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
